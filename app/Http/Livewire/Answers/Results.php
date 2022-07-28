a9467fecdb248a092e85a5113e8c578955d3cb0e
Juan Cardona <jrrricardo11@gmail.com>
2022-07-28 14:14:58 -0500

    wip: return metrics data from backend

--- app/Http/Livewire/Answers/Results.php
+++ app/Http/Livewire/Answers/Results.php
@@ -2,15 +2,40 @@
 
 namespace App\Http\Livewire\Answers;
 
-use App\Models\Participant;
+use App\Models\Dimension;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Results extends Component
 {
-    public Participant $participant;
+    public int $participantId;
+
+    public function mount()
+    {
+        $this->participantId = request()->route('participant');
+    }
 
     public function render()
     {
-        return view('livewire.answers.results')->layout('layouts.guest');
+        //$average = Dimension::query()->sum('');
+
+        return view('livewire.answers.results', [
+            'results' => DB::table('answers as a')
+                ->select('d.name as dimension')
+                ->selectRaw('count(q.id) as min')
+                ->selectRaw('sum(value) as your_score')
+                ->join('questions as q', 'q.id', 'a.question_id')
+                ->join('dimensions as d', 'd.id', 'q.dimension_id')
+                ->where('a.participant_id', $this->participantId)
+                ->groupBy('d.id')
+                ->get()
+                ->mapWithKeys(fn (\stdClass $row) => [
+                    $row->dimension => [
+                        'min' => $row->min,
+                        'max' => $row->min * 5,
+                        'your_score' => $row->your_score,
+                    ]
+                ]),
+        ])->layout('layouts.guest');
     }
 }
