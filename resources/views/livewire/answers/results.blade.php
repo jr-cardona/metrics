2cece2e12a3f4b4051a346ef763e8be8c32fca98
Juan Cardona <jrrricardo11@gmail.com>
2022-07-28 12:57:30 -0500

    refactor: rename answer question module

--- resources/views/livewire/answers/results.blade.php
+++ resources/views/livewire/answers/results.blade.php
@@ -1,5 +1,5 @@
 <div>
-    @foreach($this->participant->answerQuestions as $answer)
+    @foreach($this->participant->answers as $answer)
         <h2>{{ $answer->question->title }}</h2>
         <h2>{{ $answer->value }}</h2>
     @endforeach
