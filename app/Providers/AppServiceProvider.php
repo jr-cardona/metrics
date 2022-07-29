3fa0a55210ee08ca78269b9f4103d0fe1250d2f6
Juan Cardona <jrrricardo11@gmail.com>
2022-07-28 23:46:23 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+
         view()->composer('components.language-switcher', function ($view) {
             $view->with('currentLocale', app()->getLocale());
             $view->with('availableLocales', config('app.available_locales'));
