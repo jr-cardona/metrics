028f36299f5542489000860e7d5948c559e0a656
Juan Cardona <jr.cardona.az@gmail.com>
2022-09-17 14:32:24 -0500

    feat: adjust seeders and migrations

--- database/migrations/2022_07_15_201245_create_dimensions_table.php
+++ database/migrations/2022_07_15_201245_create_dimensions_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration {
+
     /**
      * Run the migrations.
      *
@@ -15,7 +16,7 @@ return new class () extends Migration {
         Schema::create('dimensions', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
-            $table->string('code', 5);
+            $table->text('description')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
--- database/seeders/DimensionSeeder.php
+++ database/seeders/DimensionSeeder.php
@@ -15,9 +15,22 @@ class DimensionSeeder extends Seeder
      */
     public function run(): void
     {
-        Dimension::create(['name' => 'Agresión física', 'code' => 'AF']);
-        Dimension::create(['name' => 'Agresión verbal', 'code' => 'AV']);
-        Dimension::create(['name' => 'Ira', 'code' => 'IR']);
-        Dimension::create(['name' => 'Hostilidad', 'code' => 'HO']);
+        Dimension::create([
+            'name' => 'Agresión física',
+            'description' => 'Se manifiesta a través del maltrato físico, infligiendo lesiones o daños al cuerpo de la otra persona (Solberg y Olweus, 2003).'
+        ]);
+
+        Dimension::create([
+            'name' => 'Agresión verbal',
+            'description' => 'Consiste en lanzar palabras ofensivas a la otra persona, llamarlas por calificativos despectivos, proferir amenazas, burlarse y hacer comentarios o rumores con la intención de que la otra persona se sienta ofendida (López, Sánchez, Rodríguez y Fernández, 2009).',
+        ]);
+        Dimension::create([
+            'name' => 'Ira',
+            'description' => 'Es definida como aquella sensación de disgusto causado por una ofensa o maltrato, generalmente se manifiesta a través de un deseo de combatir aquello que la origina (Weisinger, 1998).',
+        ]);
+        Dimension::create([
+            'name' => 'Hostilidad',
+            'description' => 'Entendida como todo juicio negativo hacia personas y objetos (Bus, 1961).',
+        ]);
     }
 }
