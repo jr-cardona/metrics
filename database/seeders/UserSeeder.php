8b1fa4678b3d5956a3d41ec4ec960a630581e7ed
Juan Cardona <jr.cardona.az@gmail.com>
2023-05-10 22:27:03 -0500

    fix: avoid user duplicated in user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,9 +16,10 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         if (config('app.env') !== 'production') {
-            User::create([
+            User::firstOrCreate([
                 'name' => 'Test User',
                 'email' => 'test@example.com',
+            ], [
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
